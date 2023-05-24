4153129ba311b4617addb6f82d52710ca1417e82
mac <mac@Akeems-MacBook-Air.local>
2023-05-24 22:33:27 +0100

    Dashboard and service charge

--- cypress/integration/Instanta/Instanta.test.js
+++ cypress/integration/Instanta/Instanta.test.js
@@ -1,7 +1,14 @@
 /// <reference types= "cypress" />
 
 describe("Instanta Test Suite", () => {
-    it("Validate that the Instanta app launches successfully", () => {
+    before(() => {
+        Cypress.on('uncaught:exception', () => {
+            return false
+
+        })
+
+    })
+    it("Validate that the Instanta app launches successfully", () => { 
         //Write cypress tests.
         cy.visit("https://test.facility.instanta.app")
         //Assert For every minutes shows.
@@ -11,6 +18,8 @@ describe("Instanta Test Suite", () => {
         cy.get('#username').type("admin@sysdesk.com")
         cy.get('#password').type("admin123")
         cy.get('#submitBtn').click()
+        cy.get('[href="/facility/Home.do"] > .card > .side-1 > span').click()
+        cy.get('#submenu_service_charge__wrap > .menuItem__val > #menuValLabelId').click()
     
         
     })
