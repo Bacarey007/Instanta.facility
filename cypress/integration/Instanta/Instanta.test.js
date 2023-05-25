f5817c432dcba1c8956fe7c611e6ff4090813f34
mac <mac@Akeems-MacBook-Air.local>
2023-05-25 17:30:30 +0100

    create vistor's pass

--- cypress/integration/Instanta/Instanta.test.js
+++ cypress/integration/Instanta/Instanta.test.js
@@ -18,8 +18,28 @@ describe("Instanta Test Suite", () => {
         cy.get('#username').type("admin@sysdesk.com")
         cy.get('#password').type("admin123")
         cy.get('#submitBtn').click()
+        
+        
+    
+        
+    })
+    it("Validate that the visitor's pass works successfully", () => { 
+        //Write cypress tests.
+        cy.visit("https://test.facility.instanta.app")
+        //Assert For every minutes shows.
+
+        cy.get('#username').type("abakare@sysservesolutions.com")
+        //Assert username was entered
+        cy.get('#password').type("Gunnersd12$")
+        //Asset password was entered
+        cy.get('#submitBtn').click()
         cy.get('[href="/facility/Home.do"] > .card > .side-1 > span').click()
-        cy.get('#submenu_service_charge__wrap > .menuItem__val > #menuValLabelId').click()
+        cy.get('#submenu_visitor__wrap > .menuItem__caretWrap > img').click()
+        cy.get('#submenu_visitor_0').click()
+        cy.get('.flexed > .btn > span').click()
+        cy.get(':nth-child(1) > .card__section__body > :nth-child(1) > .formInput').type('Akeem')
+        cy.get('#visitorPass_visitorType').select('Family')
+
     
         
     })
